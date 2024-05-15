c29415063696d9fbe4e08b771d000b0f8c8f0da3
priyanshu <priyanshumandloigurukul@gmail.com>
2024-05-15 21:25:02 +0530

    Making some more features

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import authRoute from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
+import dotenv from 'dotenv';
 import express from "express";
 import postRoute from "./routes/post.route.js" ;
+import testRoute from "./routes/test.route.js";
 
+dotenv.config();
 const app = express();
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
@@ -12,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800!");
