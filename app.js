7d010ebe285de27733dffcf8997dd21ae0f55cf6
priyanshu <priyanshumandloigurukul@gmail.com>
2024-05-15 17:02:16 +0530

    Authentication part is done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import authRoute from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
 import express from "express";
 import postRoute from "./routes/post.route.js" ;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello, the server is running!");
 });
