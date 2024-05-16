081efb2e521dcd62bb8460e6a7d26e795e60046a
priyanshu <priyanshumandloigurukul@gmail.com>
2024-05-16 20:13:38 +0530

    Completed the chat functionality in the application

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import authRoute from "./routes/auth.route.js"
+import chatRoute from "./routes/chat.route.js";
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import dotenv from 'dotenv';
 import express from "express";
+import messageRoute from "./routes/message.route.js";
 import postRoute from "./routes/post.route.js" ;
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
@@ -20,6 +22,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800!");
---
+++ routes/message.route.js
@@ -0,0 +1,12 @@
+import {
+  addMessage
+} from "../controllers/message.controller.js";
+import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js";
+
+const router = express.Router();
+
+
+router.post("/:chatId", verifyToken, addMessage);
+
+export default router;
