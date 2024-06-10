efa17cba182b6d4fe5e4ea685b580c95daa3edd1
priyanshu <priyanshumandloigurukul@gmail.com>
2024-06-10 19:41:28 +0530

    Changed the link

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-import authRoute from "./routes/auth.route.js"
+import authRoute from "./routes/auth.route.js";
 import chatRoute from "./routes/chat.route.js";
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from 'dotenv';
 import express from "express";
 import messageRoute from "./routes/message.route.js";
-import postRoute from "./routes/post.route.js" ;
+import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
-// const origins = "http://localhost:5173" || "https://pm-real-estate-web.vercel.app";
-// // Set up of the cors
-// app.use(cors({origin: origins,credentials:true}));
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://pm-real-estate-web.vercel.app'
+  // "http://localhost:5173",
+  "https://pm-real-estate-web.vercel.app"
 ];
 
 const corsOptions = {
@@ -27,12 +24,15 @@ const corsOptions = {
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  credentials: true
 };
 
+// Set up of the CORS
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello, the server is running!");
 });
