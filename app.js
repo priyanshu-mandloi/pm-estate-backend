20d5bb5413cc4c4bc3095eeb60309ec56e3b9200
priyanshu <priyanshumandloigurukul@gmail.com>
2024-06-10 20:03:37 +0530

    Changed the link

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
-import authRoute from "./routes/auth.route.js";
+import authRoute from "./routes/auth.route.js"
 import chatRoute from "./routes/chat.route.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import cookieParser from "cookie-parser"
+import cors from "cors"
 import dotenv from 'dotenv';
 import express from "express";
 import messageRoute from "./routes/message.route.js";
-import postRoute from "./routes/post.route.js";
+import postRoute from "./routes/post.route.js" ;
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
-
-const allowedOrigins = [
-  // "http://localhost:5173",
-  "https://pm-real-estate-web.vercel.app"
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
-
-// Set up of the CORS
-app.use(cors(corsOptions));
+// Set up of the cors
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
-
 app.get("/", (req, res) => {
   res.send("Hello, the server is running!");
 });
