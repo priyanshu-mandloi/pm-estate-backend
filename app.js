701bf247066a33bb4f482e853003f4e7b9c1f789
priyanshu <priyanshumandloigurukul@gmail.com>
2024-06-13 14:49:26 +0530

    Changed the link

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ dotenv.config();
 const app = express();
 // Set up of the cors
 app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
