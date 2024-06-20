0d4f5e2a7e9d7a53e2925b8ce60a43ce5658a289
priyanshu <priyanshumandloigurukul@gmail.com>
2024-06-21 00:52:17 +0530

    Resolve conflicts

--- routes/message.route.js
+++ routes/message.route.js
@@ -2,11 +2,12 @@ import {
   addMessage
 } from "../controllers/message.controller.js";
 import express from "express";
-import {verifyToken} from "../middleware/verifyToken.js";
+
+// import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 
-router.post("/:chatId", verifyToken, addMessage);
+router.post("/:chatId", addMessage);
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -9,17 +9,18 @@ import {
 } from "../controllers/user.controller.js";
 
 import express from "express";
-import {verifyToken} from "../middleware/verifyToken.js";
+
+// import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 // router.get("/:id", getUser);
 // router.get("/search/:id", verifyToken, getUser);
-router.put("/:id", verifyToken, updateUser);
-router.delete("/:id", verifyToken, deleteUser);
-router.post("/save", verifyToken, savePost);
-router.get("/profilePosts", verifyToken, profilePosts);
-router.get("/notification", verifyToken, getNotificationNumber);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.post("/save",  savePost);
+router.get("/profilePosts",  profilePosts);
+router.get("/notification",  getNotificationNumber);
 
 export default router;
